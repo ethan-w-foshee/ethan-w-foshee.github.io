46de8186a5297b6aaafd21f84992c319356eeb86
ethan.w.foshee <skyhailer2@gmail.com>
2022-10-13 19:07:31 -0500

    Permanent Nav bar created and made on all pages!

--- scripts/nav-bar.js
+++ scripts/nav-bar.js
@@ -5,3 +5,35 @@ let openNav = () => {
     let button = document.getElementById("tapBox")
     button.classList.toggle("tapBoxOpen")
 }
+
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+        <nav>
+            <a href="../index.html" id="logo"><img
+                    src="https://cdn.evbuc.com/eventlogos/188084824/acastandardcirclefullname.png"
+                    alt="austin coding academy logo"></a>
+            <a id="has-border-darker" class="navButton" href="../pages/resume.html">Resume</a>
+            <a id="has-border-dark" class="navButton" href="../pages/about.html">About Me</a>
+            <a id="has-border-lighter" class="navButton" href="https://medium.com/@ethan.w.foshee">My Blog</a>
+            <a id="has-border-light" class="navButton" href="../pages/contact.html">Contact Me</a>
+        </nav>
+        <div id="tapBox" onclick="openNav()"></div>
+        </header>
+        `
+    }
+}
+
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer>
+            <a class="small-text" href="https://austincodingacademy.com/">© Austin Coding Academy</a>
+        </footer>
+        `
+    }
+}
+
+customElements.define('main-header', Header)
+customElements.define('main-footer', Footer)
